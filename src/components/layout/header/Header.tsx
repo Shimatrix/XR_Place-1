6596da2bc6650f9202d3f228e25422ca40696bdc
Anna Kozachenko <annakozacenko@yandex.ru>
2025-04-30 20:44:44 +0300

    добавлена основа шапки

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,15 +1,31 @@
 import styles from './Header.module.scss';
+import logoImage from '../../../assets/images/logo.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>XRPlace</h1>
+      {/* Логотип */}
+      <div className={styles.logo}>
+        <img src={logoImage} alt='Логотип' className={styles.logoImage} />
+      </div>
+
+      {/* Меню и переключение языка */}
+      <div className={styles.menu}>
         <nav className={styles.nav}>
-          <a href='#features'>Возможности</a>
-          <a href='#demo'>Демо</a>
-          <a href='#contacts'>Контакты</a>
+          <a href='#'>О КОМПАНИИ</a>
+          <span className={styles.divider} />
+          <a href='#'>КАК ЭТО РАБОТАЕТ</a>
+          <span className={styles.divider} />
+          <a href='#'>ВОЗМОЖНОСТИ</a>
         </nav>
+
+        {/* Переключение языка */}
+        <div className={styles.languageSwitcher}>
+          <div className={styles.activeDot} />
+          <span className={styles.inactive}>EN</span>
+          <span className={styles.divider} />
+          <span className={styles.active}>RU</span>
+        </div>
       </div>
     </header>
   );
