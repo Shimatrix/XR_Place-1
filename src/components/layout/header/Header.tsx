e6491bedae832a0759d789c01d6dbe6bac3badfc
Anna Kozachenko <annakozacenko@yandex.ru>
2025-05-01 00:00:14 +0300

    style(header): update styles and improve layout for header component

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -4,22 +4,19 @@ import logoImage from '../../../assets/images/logo.svg';
 export function Header() {
   return (
     <header className={styles.header}>
-      {/* Логотип */}
       <div className={styles.logo}>
         <img src={logoImage} alt='Логотип' className={styles.logoImage} />
       </div>
 
-      {/* Меню и переключение языка */}
       <div className={styles.menu}>
         <nav className={styles.nav}>
           <a href='#'>О КОМПАНИИ</a>
-          <span className={styles.divider} />
+          <span className={`${styles.divider} ${styles.firstDivider}`} />
           <a href='#'>КАК ЭТО РАБОТАЕТ</a>
-          <span className={styles.divider} />
+          <span className={`${styles.divider} ${styles.secondDivider}`} />
           <a href='#'>ВОЗМОЖНОСТИ</a>
         </nav>
 
-        {/* Переключение языка */}
         <div className={styles.languageSwitcher}>
           <div className={styles.activeDot} />
           <span className={styles.inactive}>EN</span>
