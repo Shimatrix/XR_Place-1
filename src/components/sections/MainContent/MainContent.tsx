10f225e5bf2076df4dc356db0a1ef86c02b1e1ec
Anastasia Korshunova <gatta.gizmo@gmail.com>
2025-05-03 12:18:25 +0300

    Merge develop into feat/business-team, resolved conflicts

--- src/components/sections/MainContent/MainContent.tsx
+++ src/components/sections/MainContent/MainContent.tsx
@@ -1,29 +1,33 @@
 import styles from './MainContent.module.scss';
-import { Title, TitleHighlight } from '../../ui/h2/Title';
+// import { Title, TitleHighlight } from '../../ui/h2/Title';
+import { BusinessTeam } from './BusinessTeam';
 
 export function MainContent() {
   return (
     <div className={styles.container}>
-      <section className={styles.hero}>
-        <h1>3D Туры по будущей недвижимости</h1>
-        <p>Покажите покупателям квартиру ещё до постройки дома</p>
-        <button className={styles.ctaButton}>Заказать демо</button>
-      </section>
+      {/*  <section className={styles.hero}>
+    //     <h1>3D Туры по будущей недвижимости</h1>
+    //     <p>Покажите покупателям квартиру ещё до постройки дома</p>
+    //     <button className={styles.ctaButton}>Заказать демо</button>
+    //   </section> */}
 
-      <section className={styles.features}>
-        <Title label='сотрудничество'>
-          Реализованые <TitleHighlight>проекты</TitleHighlight>
-        </Title>
-        <div className={styles.featuresGrid}>
-          <div className={styles.featureCard}>
-            <h3>Вид из окна</h3>
-            <p>Реалистичная визуализация окружения</p>
-          </div>
-          <div className={styles.featureCard}>
-            <h3>Свобода перемещения</h3>
-            <p>Навигация как в компьютерной игре</p>
-          </div>
-        </div>
+      {/* //   <section className={styles.features}>
+    //     <Title label='сотрудничество'>
+    //       Реализованые <TitleHighlight>проекты</TitleHighlight>
+    //     </Title>
+    //     <div className={styles.featuresGrid}>
+    //       <div className={styles.featureCard}>
+    //         <h3>Вид из окна</h3>
+    //         <p>Реалистичная визуализация окружения</p>
+    //       </div>
+    //       <div className={styles.featureCard}>
+    //         <h3>Свобода перемещения</h3>
+    //         <p>Навигация как в компьютерной игре</p>
+    //       </div>
+    //     </div>
+    //   </section> */}
+      <section>
+        <BusinessTeam />
       </section>
     </div>
   );
