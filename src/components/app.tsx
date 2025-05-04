0d84346b228838be1325c214f073fb475da10905
Anastasia <gatta.gizmo@gmail.com>
2025-05-04 14:43:32 +0300

    Merge develop into feat/business-team, resolved conflicts (#8)
    Добавлен блок "Бизнес Команда"
    
    Что сделано:
    Реализована секция "Бизнес Команда" с поддержкой адаптивной вёрстки с карточками участников команды (Артём Тарханов и Виктор Карнеев) и описательными текстовыми карточками.
    
    Реализована поддержка как десктопной, так и мобильной версии:
    
    Карточки адаптируются по ширине;
    Декоративные элементы корректно позиционируются в обоих режимах.
    Добавлены анимации появления контента.
    Использована SCSS-архитектура с переменными из variables.css.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,12 @@
 import { Layout } from './ui';
 import { CardsBlock } from './sections/cardProject/cardBlock';
+import { BusinessTeam } from './sections/BusinessTeam/BusinessTeam';
 
 export function App() {
   return (
     <Layout>
       <CardsBlock />
+      <BusinessTeam />
     </Layout>
   );
 }
