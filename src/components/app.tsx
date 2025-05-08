2203b2b5b3f9e94fa3ed6727d5bd3dbc1f17839d
Pavel Zavritskiy <158479431+PavelZavritskiy@users.noreply.github.com>
2025-05-08 09:46:09 +0300

    Добавил секцию Как это работает, переделал слегка кнопку, сделал возможность добавлять другую иконку, оставил дефолтное значение для иконки. (#21)

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,7 +7,9 @@ import { WidgetSection } from './sections/Widget/Widget';
 import 'react-phone-number-input/style.css';
 import { CustomCursor } from './CustomCursor/CustomCursor';
 import { FAQBlock } from './sections/faq/FAQBlock';
+import HowItWorks from './sections/how_it_works/HowItWorks';
 import { PartnersBlock } from './sections//PartnersBlock/PartnerBlock';
+
 
 export function App() {
   return (
@@ -17,6 +19,7 @@ export function App() {
       <PartnersBlock />
       <ClientsTrust />
       <CardsBlock />
+      <HowItWorks />
       <BusinessTeam />
       <FAQBlock />
       <WidgetSection />
