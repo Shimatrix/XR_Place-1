dcfaabe219ceb502d18bd584fc5d802a84de50fd
GrigoriTutovchik <151522965+GrigoriTutovchik@users.noreply.github.com>
2025-05-08 09:44:23 +0300

    Revert "Partners block (#20)" (#22)
    This reverts commit 3aad1afbf605adba041d612eb1aca14ee723806a.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/app';
 import Providers from './providers';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
