fb84e4015114c483bc2164f18ddef533befba8aa
GrigoriTutovchik <151522965+GrigoriTutovchik@users.noreply.github.com>
2025-05-14 19:23:25 +0300

    Revert "Partners block (#40)" (#47)
    This reverts commit c8f3f9baa983b4b7edc5f7f80011e05c12f360dd.

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,40 +1,15 @@
 import styles from './Header.module.scss';
-import logoImage from '../../../assets/images/logo.svg';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src={logoImage} alt='Логотип' className={styles.logoImage} />
-      </div>
-
-      <div className={styles.menu}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>XRPlace</h1>
         <nav className={styles.nav}>
-          <a href='#'>О КОМПАНИИ</a>
-          <span className={`${styles.divider} ${styles.firstDivider}`} />
-          <a href='#'>КАК ЭТО РАБОТАЕТ</a>
-          <span className={`${styles.divider} ${styles.secondDivider}`} />
-          <a href='#'>ВОЗМОЖНОСТИ</a>
+          <a href='#features'>Возможности</a>
+          <a href='#demo'>Демо</a>
+          <a href='#contacts'>Контакты</a>
         </nav>
-
-        <div className={styles.languageSwitcher}>
-          <div className={styles.activeDot} />
-          <button
-            className={styles.inactive}
-            type='button'
-            aria-label='Switch to English'
-          >
-            EN
-          </button>
-          <span className={styles.divider} />
-          <button
-            className={styles.active}
-            type='button'
-            aria-label='Переключить на русский'
-          >
-            RU
-          </button>
-        </div>
       </div>
     </header>
   );
