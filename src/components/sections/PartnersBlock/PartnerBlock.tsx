da84431291bfd0fc03f5b6b7e2618e41cf2b5f96
ArteWolfves <artewolfves.o.o@gmail.com>
2025-05-15 01:30:07 +0700

    add mobile version

--- src/components/sections/PartnersBlock/PartnerBlock.tsx
+++ src/components/sections/PartnersBlock/PartnerBlock.tsx
@@ -4,13 +4,15 @@ import styles from './PartnerBlock.module.scss';
 
 export const PartnersBlock = () => (
   <section className={styles.partnerBlock}>
-    <TitlePartner label='НАША ФИЛОСОФИЯ'>
-      <div className={styles.animationTitle}> </div>надёжный партнер{' '}
-      <TitleHighlight>УСПЕХ</TitleHighlight> бизнеса
-    </TitlePartner>
-    {/* ДЕСКТОП */}
-    <div className={styles.scrollLine} />
     <div className={styles.desktopLayoutWrapper}>
+      <div className={styles.animationTitle}>
+        <TitlePartner label='НАША ФИЛОСОФИЯ'>
+          надёжный партнер
+          <TitleHighlight> успех </TitleHighlight> бизнеса
+        </TitlePartner>
+      </div>
+      {/* ДЕСКТОП */}
+      <div className={styles.scrollLine} />
       <div className={styles.desktopGrid}>
         {/* ЛЕВАЯ КОЛОНКА — ФОТОГРАФИЯ */}
         <div className={styles.blockImg}>
@@ -55,6 +57,42 @@ export const PartnersBlock = () => (
           />
         </div>
         {/* <div className={styles.scrollLine}></div> */}
+      </div>
+    </div>
+
+    {/* МОБИЛЬНАЯ ВЕРСИЯ */}
+    <div className={styles.mobileLayoutWrapper}>
+      <div className={styles.mobileGrid}>
+        <TitlePartner label='НАША ФИЛОСОФИЯ'>
+          <div className={styles.mobileTitle}>
+            {' '}
+            надёжный партнер
+            <TitleHighlight> успех бизнеса </TitleHighlight>
+          </div>
+        </TitlePartner>
+        {/* ОПИСАНИЕ */}
+        <div className={styles.mobileDescription}>
+          <p>
+            Мы предлагаем уникальный виджет для создания виртуальных туров,
+            которые позволяют вашим клиентам исследовать объекты недвижимости в
+            интерактивном формате.
+          </p>
+        </div>
+        {/* ИЗОБРАЖЕНИЕ */}
+        <div className={styles.mobileImg}>
+          <img
+            src='../src/assets/images/room.jpg'
+            alt='фотография комнаты'
+            className={styles.roomImg}
+          />
+        </div>
+        {/* ПОДЗАГОЛОВОК */}
+        <div className={styles.mobileSubTitleWrapper}>
+          <p className={styles.subTitle}> преобразуйте свою</p>
+          <p className={styles.subTitle}> реальность</p>
+          <p className={styles.subTitle}> с интерактивными </p>
+          <p className={styles.subTitle}> виртуальными 3D турами</p>
+        </div>
       </div>
     </div>
   </section>
